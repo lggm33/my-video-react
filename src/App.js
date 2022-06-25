4502b9806dd82e656ff3f97bd1d46593184756e4
unknown <you@example.com>
2022-06-24 18:14:06 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
-import {connect, createLocalTracks} from 'twilio-video'
+import { connect } from 'twilio-video'
 import './App.css';
 
 function App() {
@@ -11,40 +11,105 @@ function App() {
     token: '',
     roomName: '',
     identity: '',
+    room: null,
+
   })
+
+  const videoParticipants = useRef(null)
+
+  const handleSubmit = async () => {
+    getToken()
+  }
 
   const getToken = async () => {
     try {
       const response = await axios.post(getTokenURL, {
-        identity: state.identity, roomName:state.roomName})
-      setState({...state, token:response.data.jwt })
+        identity: state.identity, roomName: state.roomName
+      })
+      setState({ ...state, token: response.data.jwt })
       console.log(response.data.jwt)
     } catch (error) {
       console.log(error)
     }
-    
+
   }
 
-  const handleSubmit = async () => {
-    getToken()
-  } 
 
-   const handleOnChange = (e) => {
-    const {name, value} = e.target
+  useEffect(() => {
+    if (state.room) {
 
-    setState({...state, [name]: value})
-   }
+      handleConnectedParticipant(state.room.localParticipant)
+      state.room.participants.forEach(handleConnectedParticipant)
+      state.room.on('participantConnected', handleConnectedParticipant)
 
-   useEffect(() => {console.log(state)}, [state])
+      state.room.on('participantDisconnected', handleDisconnectedParticipant)
+      window.addEventListener('pagehide', () => state.room.disconnect())
+      window.addEventListener('beforeunload', () => state.room.disconnect())
+    }
+
+
+  }, [state.room])
+
+  const handleConnectedParticipant = (participant) => {
+    participant.tracks.forEach((trackPublication) => {
+      handleTrackPublished(trackPublication, participant)
+    })
+    participant.on('trackPublished', (trackPublication, participant) => {
+      handleTrackPublished(trackPublication, participant)
+    })
+  }
+
+  const handleTrackPublished = (trackPublication, participant) => {
+
+    const videoParticipantDiv = document.createElement("div");
+    videoParticipantDiv.id = participant.sid
+    videoParticipants.current.appendChild(videoParticipantDiv)
+    const handleTrackSubcribed = (track) => { videoParticipantDiv.appendChild(track.attach()) }
+
+    if (trackPublication.track) { handleTrackSubcribed(trackPublication.track) }
+    trackPublication.on('subscribed', handleTrackSubcribed)
+  }
+
+  const handleDisconnectedParticipant = (participant) => {
+    const participantToRemove = document.getElementById(`${participant.sid}`)
+    participantToRemove.remove()
+  }
+
+  useEffect(() => {
+
+    const joinRoom = async () => {
+      try {
+        // const mediaTracks = await createMediaTracks()
+        const room = await connect(state.token, { name: state.roomName, video: true, audio: false })
+        setState({ ...state, room })
+      } catch (error) {
+        console.log(`Unable to connect to Room: ${error}`)
+      }
+    }
+
+    if (state.token !== '') {
+      joinRoom()
+    }
+
+  }, [state.token])
+
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+
+    setState({ ...state, [name]: value })
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>State.token: {state.token}</p>
+
+      <p>State.token: {state.token}</p>
       <button type='button' onClick={(e) => handleSubmit(e)}>Get Token</button>
       <input name='identity' placeholder='User' value={state.identity} onChange={(e) => handleOnChange(e)}></input>
       <input name='roomName' placeholder='Room' value={state.roomName} onChange={(e) => handleOnChange(e)}></input>
-      </header>
+      <div ref={videoParticipants} ></div>
+
+
 
     </div>
   );
